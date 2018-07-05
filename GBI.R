35ccabd31aad69fcfbc5b69e2b172c6aef0f3a3c
JepsonNomad <cjohn@ucdavis.edu>
2018-07-05 15:57:10 -0700

    Updates to readme and adds forecast function to code

--- GBI.R
+++ GBI.R
@@ -1,5 +1,6 @@
 library(RCurl)
 library(ggplot2)
+library(forecast)
 
 # Download GL Blocking Index data
 myfile <- getURL("https://www.esrl.noaa.gov/psd/data/correlation/gbi.ncep.day")
@@ -23,6 +24,7 @@ GBI$Date
 GBI <- GBI[!is.na(GBI$Date),] 
 # Aggregate by year
 GBI.year <- aggregate(GBI, by=list(GBI$Year), FUN = mean, na.rm=T)
+GBI.year <- GBI.year[,c("GBI","Date")]
 GBI.year
 str(GBI.year)
 # Aggregate by month-year
@@ -30,14 +32,42 @@ GBI$YrMo <- paste(GBI$Year,
                   stringr::str_pad(GBI$Month, pad="0",width=2), 
                   sep="-")
 GBI.yrmo <- aggregate(GBI, by=list(GBI$YrMo), FUN = mean, na.rm=T)
+GBI.yrmo <- GBI.yrmo[,c("Date","GBI")]
 str(GBI.yrmo)
-
+# Look at GBI during months leading up to green-up
+GBI.sprng <- GBI[as.numeric(levels(GBI$Month)[GBI$Month]) < 7,]
+GBI.sprng
+GBI.annsprngs <- aggregate(GBI.sprng,
+                           by=list(GBI.sprng$Year),
+                           FUN = mean,
+                           na.rm = T)
+GBI.annsprngs <- GBI.annsprngs[,c("GBI","Date")]
 # Plot the results
 # Here, daily, monthly, and yearly GBI values
 ggplot() +
   geom_line(data=GBI,aes(x=Date,y=GBI), 
             colour="black") +
   geom_line(data=GBI.moyr, aes(x=Date, y = GBI), 
-            colour="red", lwd=0.5) +
+            colour="red3", lwd=0.5) +
   geom_line(data=GBI.year, aes(x=Date, y = GBI), 
-            colour="red", lwd=2)
+            colour="blue3", lwd=2) +
+  geom_line(data=GBI.annsprngs, aes(x=Date, y = GBI), 
+            colour="green3", lwd=2)
+
+# Time series analysis
+GBI.ts <- ts(GBI$GBI,
+             start = as.numeric(format(min(GBI$Date), "%Y")),
+             end = as.numeric(format(max(GBI$Date), "%Y")),
+             frequency = 365)
+GBI.ts
+acf(GBI.ts)
+# Forecasting
+GBI.ts.tbats <- tbats(GBI.ts)
+plot(x.ts.tbats)
+x.ts.tbats
+# Forecast the data for ~5 years ahead
+x.ts.fore <- forecast(x.ts.tbats, h=7200)
+plot(x.ts.fore)
+x.ts.fore
+
+
--- ReadMe.md
+++ ReadMe.md
@@ -1,7 +1,7 @@
 # GBI
 
 ## Intent
-This is a short block of code to download and investigate the Greenland Blocking Index (GBI). Data is extracted from https://www.esrl.noaa.gov/psd/data/correlation/gbi.ncep.day. The GBI is a measure of high-pressure blocking expressed as meters above sea level, reflecting the mean geopotential height of 500 hPa over the region bounded by 60-80 deg N and 20-80 deg W. For further information see Hanna et al 2016 Int J Climatology.
+This is a short block of R code to download and investigate the Greenland Blocking Index (GBI). Data is extracted from https://www.esrl.noaa.gov/psd/data/correlation/gbi.ncep.day. The GBI is a measure of high-pressure blocking expressed as meters above sea level, reflecting the mean geopotential height of 500 hPa over the region bounded by 60-80 deg N and 20-80 deg W. For further information see Hanna et al 2016 Int J Climatology.
 
 ## Dependencies
 Code uses RCurl and ggplot2 to to download and visualise the data.
